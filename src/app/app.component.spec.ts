cda9577d06ecfff01051cf784ef5299f56b190c9
Harshit Gupta <kohthecodemaster@gmail.com>
2024-06-26 13:19:38 +0530

    3rd Commit - Refactor - Google-Keep-Clone

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Google-Keep' title`, () => {
+  it(`should have the 'Google-Keep-Clone' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Google-Keep');
+    expect(app.title).toEqual('Google-Keep-Clone');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Google-Keep');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Google-Keep-Clone');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Google-Keep';
+  title = 'Google-Keep-Clone';
 }
